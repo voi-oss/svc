46902e47888c53a4327cd0b9344c55a1249c5322
Uwe Dauernheim <djui@users.noreply.github.com>
2019-10-28 23:57:42 +0100

    Replace Gin with http.ServeMux

--- options.go
+++ options.go
@@ -1,12 +1,12 @@
 package svc
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/pprof"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.uber.org/zap"
@@ -26,29 +26,9 @@ func WithTerminationGracePeriod(d time.Duration) Option {
 	}
 }
 
-// WithProductionRouter is an option that uses Gin as HTTP router in release
-// mode.
-func WithProductionRouter() Option {
-	return func(s *SVC) error {
-		// Has to be done before `gin.New()` and overrides `GIN_MODE` env!
-		gin.SetMode(gin.ReleaseMode)
-		return WithRouter(gin.New())(s)
-	}
-}
-
-// WithDevelopmentRouter is an option that uses Gin as HTTP router in debug
-// mode.
-func WithDevelopmentRouter() Option {
-	return func(s *SVC) error {
-		// Has to be done before `gin.New()` and overrides `GIN_MODE` env!
-		gin.SetMode(gin.DebugMode)
-		return WithRouter(gin.New())(s)
-	}
-}
-
-// WithRouter is an option that replaces the HTTP router with the given Gin
+// WithRouter is an option that replaces the HTTP router with the given http
 // router.
-func WithRouter(router *gin.Engine) Option {
+func WithRouter(router *http.ServeMux) Option {
 	return func(s *SVC) error {
 		s.Router = router
 		return nil
@@ -95,8 +75,7 @@ func assignLogger(s *SVC, logger *zap.Logger, atom zap.AtomicLevel) error {
 // log level.
 func WithLogLevelHandlers() Option {
 	return func(s *SVC) error {
-		s.Router.GET("/loglevel", gin.WrapH(s.atom))
-		s.Router.PUT("/loglevel", gin.WrapH(s.atom))
+		s.Router.Handle("/loglevel", s.atom)
 
 		return nil
 	}
@@ -137,7 +116,7 @@ func WithMetrics() Option {
 //Prometheus scraper.
 func WithMetricsHandler() Option {
 	return func(s *SVC) error {
-		s.Router.GET("/metrics", gin.WrapH(promhttp.Handler()))
+		s.Router.Handle("/metrics", promhttp.Handler())
 
 		return nil
 	}
@@ -148,18 +127,18 @@ func WithMetricsHandler() Option {
 func WithPProfHandlers() Option {
 	return func(s *SVC) error {
 		// See https://github.com/golang/go/blob/master/src/net/http/pprof/pprof.go#L72-L77
-		s.Router.GET("/debug/pprof/", gin.WrapF(pprof.Index))
-		s.Router.GET("/debug/pprof/cmdline", gin.WrapF(pprof.Cmdline))
-		s.Router.GET("/debug/pprof/profile", gin.WrapF(pprof.Profile))
-		s.Router.GET("/debug/pprof/symbol", gin.WrapF(pprof.Symbol))
-		s.Router.GET("/debug/pprof/trace", gin.WrapF(pprof.Trace))
+		s.Router.HandleFunc("/debug/pprof/", pprof.Index)
+		s.Router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+		s.Router.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		s.Router.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+		s.Router.HandleFunc("/debug/pprof/trace", pprof.Trace)
 		// See https://github.com/golang/go/blob/master/src/net/http/pprof/pprof.go#L248-L258
-		s.Router.GET("/debug/pprof/allocs", gin.WrapH(pprof.Handler("allocs")))
-		s.Router.GET("/debug/pprof/block", gin.WrapH(pprof.Handler("block")))
-		s.Router.GET("/debug/pprof/goroutine", gin.WrapH(pprof.Handler("goroutine")))
-		s.Router.GET("/debug/pprof/heap", gin.WrapH(pprof.Handler("heap")))
-		s.Router.GET("/debug/pprof/mutex", gin.WrapH(pprof.Handler("mutex")))
-		s.Router.GET("/debug/pprof/threadcreate", gin.WrapH(pprof.Handler("threadcreate")))
+		s.Router.Handle("/debug/pprof/allocs", pprof.Handler("allocs"))
+		s.Router.Handle("/debug/pprof/block", pprof.Handler("block"))
+		s.Router.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+		s.Router.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+		s.Router.Handle("/debug/pprof/mutex", pprof.Handler("mutex"))
+		s.Router.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
 
 		return nil
 	}
@@ -170,12 +149,13 @@ func WithPProfHandlers() Option {
 func WithHealthz() Option {
 	return func(s *SVC) error {
 		// Register live probe handler
-		s.Router.GET("/live", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{"status": "Still Alive!"})
+		s.Router.HandleFunc("/live", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			_, _ = w.Write([]byte(`{"status": "Still Alive!"}`))
 		})
 
 		// Register ready probe handler
-		s.Router.GET("/ready", func(c *gin.Context) {
+		s.Router.HandleFunc("/ready", func(w http.ResponseWriter, r *http.Request) {
 			var errs []error
 			for n, w := range s.workers {
 				if hw, ok := w.(Healther); ok {
@@ -186,10 +166,15 @@ func WithHealthz() Option {
 			}
 			if len(errs) > 0 {
 				s.logger.Warn("Ready check failed", zap.Any("errs", errs))
-				c.JSON(http.StatusServiceUnavailable, gin.H{"errors": errs})
-				return
+				b, err := json.Marshal(map[string]interface{}{"errors": errs})
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				w.WriteHeader(http.StatusServiceUnavailable)
+				w.Header().Set("Content-Type", "application/json")
+				_, _ = w.Write(b)
 			}
-			c.Status(http.StatusOK)
 		})
 
 		return nil
